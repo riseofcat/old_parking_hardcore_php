0cdabad4d36d6b61f800c9e950347f0884a9059e
dim <avdim@mail.ru>
2016-10-03 06:16:14 +0300

    deletted

--- murrr/stat/ls.php
+++
@@ -1,6 +0,0 @@
-<?php
-$file = fopen("visit.txt", "a+");
-date_default_timezone_set("Europe/Moscow");
-fwrite($file, date('H:i:s d.m.Y') . "  " . $_POST["agent"] . "  " . $_POST["ip"]);
-fclose($file);
-?>
